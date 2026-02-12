871524d2855edcd4cdd78ea921d1e5714c45c295
bhavikpatidar7 <bhavikpatidar1007@gmail.com>
2026-02-12 22:56:51 +0530

    correction

---
+++ backend/routes/storeRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../config/db");
+const { verifyToken, checkRole } = require("../middleware/authMiddleware");
+
+router.get("/", verifyToken, checkRole("OWNER"), (req, res) => {
+    res.json({ message: "Store Route Working" });
+});
+
+
+router.get("/storeRoutes", verifyToken, checkRole("OWNER"), (req, res) => {
+    db.query(
+        `SELECT users.name, ratings.rating 
+         FROM ratings 
+         JOIN users ON ratings.user_id = users.id
+         JOIN stores ON ratings.store_id = stores.id
+         WHERE stores.owner_id=?`,
+        [req.user.id],
+        (err, result) => {
+            res.json(result);
+        }
+    );
+});
+module.exports = router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../config/db");
+const { verifyToken, checkRole } = require("../middleware/authMiddleware");
+
+// Submit or Update Rating
 router.post("/rate", verifyToken, checkRole("USER"), (req, res) => {
     const { store_id, rating } = req.body;
 
     db.query(
-        "INSERT INTO ratings (user_id, store_id, rating) VALUES (?,?,?) ON DUPLICATE KEY UPDATE rating=?",
+        `INSERT INTO ratings (user_id, store_id, rating)
+         VALUES (?, ?, ?)
+         ON DUPLICATE KEY UPDATE rating = ?`,
         [req.user.id, store_id, rating, rating],
-        (err) => {
+        (err, result) => {
             if (err) return res.status(500).json(err);
-            res.json({ message: "Rating submitted" });
+            res.json({ message: "Rating submitted successfully" });
         }
     );
 });
+
+module.exports = router;
